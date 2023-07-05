8def337f0aadca2725a9728d75e8df06686ba1fe
Ethan Mayer <ethan.mayer@comcast.net>
2023-07-05 14:20:03 -0500

    CLI arguments format fix

--- src/driver.py
+++ src/driver.py
@@ -14,19 +14,22 @@ import reliability_tests
 import subprocess
 
 # Handle command-line arguments
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(prog = "Reliability Tests", description = "This program is designed to run tests on the reliability of payload electromechanical systems.")
 
 # CMDline argument for time
 parser.add_argument("-T", "--time", type = int, help = "Length of time to run the tests in minutes (int)")
 
+# CMDline argument for stress
+parser.add_argument("-S", "--stress", help = "Whether to stress the CPU, I/O, and Memory to 100%%", action = "store_true")
+
 # CMDline argument for verbose
 parser.add_argument("-V", "--verbose", help = "Whether to print data to the terminal ONLY (no logging)", action = "store_true")
 
-# CMDline argument for stress
-parser.add_argument("-S", "--stress", help = "Whether to stress the CPU, I/O, and Memory to 100%", action = "store_true")
-
 # Parse the provided arguments
 args = parser.parse_args()
+
+if args.verbose:
+    print("VERBOSE")
 
 # Create the test object
 tests = reliability_tests.reliability_tests()
